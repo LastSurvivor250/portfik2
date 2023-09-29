273b8eae20b9e2e26178d88c5d909a970f56eee9
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2023-09-29 20:19:01 +0200

    x

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export default async function connectToDB() {
   try {
     await mongoose.connect(
-      "mongodb+srv://sangam:sangam2023@cluster0.7aauitj.mongodb.net/"
+      "mongodb+srv://migasiuk250:7fPaG0ayAAxH2MNy@onlineshop.a8l5lxo.mongodb.net/"
     );
     console.log("Database connected successfully");
   } catch (e) {
